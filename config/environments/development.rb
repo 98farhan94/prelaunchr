33b5db7566d83d461caa4fc76956fe3ab292a839
98farhan94 <98farhan94@gmail.com>
2016-03-23 16:39:46 +0300

    Added files via upload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,6 @@ Prelaunchr::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
-  config.serve_static_assets = true
 
   # Expands the lines which load the assets
   config.assets.debug = true
