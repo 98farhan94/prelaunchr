520d29e1fb37ee67a23d3b47ed51764418f41d0f
Shaikh Farhan <Farhan@Farhans-MacBook-Pro.local>
2016-03-28 22:42:06 +0300

    Mailer config...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,17 @@ config.action_mailer.raise_delivery_errors = true
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-    config.action_mailer.default_url_options = { :host => 'joeypad.heroku.com' }
+#config.action_mailer.default_url_options = { :host => 'joeypad.herokuapp.com' }
+    
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
